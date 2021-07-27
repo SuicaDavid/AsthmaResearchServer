0e8d9259477deebaad0ced9b0205066b8f986ca9
SuicaDavid <genglanghuang@gmail.com>
2021-07-27 22:15:02 +0100

    refine the request

--- routes/weaters.js
+++ routes/weaters.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const axios = require('axios')
+const weather = require('../models/weather')
 const router = express.Router()
 
 const WEATHER_API_KEY =  process.env.WEATHER_API_KEY
@@ -8,9 +9,13 @@ router.post('/all', (req, res)=>{
     const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`
     axios({ url })
     .then(data=>{
-        console.log(data.data)
-        res.json(data)
-        // Weather.create({})
+        const result = data.data
+        weather.create(result)
+        setTimeout(async ()=>{
+            let savedWeather = await weather.findOne({name: 'Abbey Wood'}).exec()
+            console.log(savedWeather)
+        },1000)
+        res.json(data.data)
     })
     .catch(error=> {
         console.error(error)
