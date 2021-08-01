aa829e5a8381aa5777db824337bbca41ca135ed4
SuicaDavid <genglanghuang@gmail.com>
2021-08-02 00:06:45 +0100

    Fixed the bug of socket

--- routes/weaters.js
+++ routes/weaters.js
@@ -3,25 +3,32 @@ const axios = require('axios')
 const weather = require('../models/weather')
 const {setUTCTime} = require('../utility/dateUtility')
 const router = express.Router()
+const cityListJson = require('../city.list.json')
+const https = require('https')
+const { url } = require('inspector')
 
 const WEATHER_API_KEY =  process.env.WEATHER_API_KEY
 const MS_PER_MINUTE = 60000
 const REQUEST_INTERVAL = 15 * MS_PER_MINUTE
 
-
-
+const instance = axios.create({
+    httpsAgent: new https.Agent({keepAlive: true}),
+    timeout: 500
+})
 router.get('/', async (req, res)=>{
     req.on('error',function(err){console.log(err)})
-    let savedWeather = await weather.findOne({id: req.query.cityId}).exec()
+    let city = cityListJson.find(city=>city.name === req.query.cityName)
+    req.city = city
+    let savedWeather = await weather.findOne({id: city.id}).exec()
     if(savedWeather) {
         let now = setUTCTime()
         if ((now - savedWeather.updatedDate) > REQUEST_INTERVAL) {
             console.log("Renew")
             fetchWeatherByCityId(req, res)
                 .then(result=>{
-                    savedWeather.replaceOne({id: req.query.cityId}, result)
+                    savedWeather.replaceOne({id: city.id}, result)
                     res.json(result)
-                })
+                })          
         } else {
             console.log("Cache")
             res.json(savedWeather)
@@ -38,8 +45,8 @@ router.get('/', async (req, res)=>{
 
 function fetchWeatherByCoordination(req, res) {
     const {lat, lon} = req.query
-    const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`
-    return axios({ url })
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`
+    return instance.get(url)
     .then(data=>{
         const result = data.data
         return result
@@ -49,11 +56,10 @@ function fetchWeatherByCoordination(req, res) {
         res.status(500).json({message: error.message})
     })
 }
-
 function fetchWeatherByCityId(req, res) {
-    const {cityId} = req.query
-    const url = `http://api.openweathermap.org/data/2.5/weather?id=${cityId}&appid=${WEATHER_API_KEY}`
-    return axios({ url })
+    const {id} = req.city
+    const url = `https://api.openweathermap.org/data/2.5/weather?id=${id}&appid=${WEATHER_API_KEY}`
+    return instance.get(url)
         .then(data=>{
             const result = data.data
             return result
